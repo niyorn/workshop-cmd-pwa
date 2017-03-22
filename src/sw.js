ae2774a14249bef0a15cf3c837ff4e7555a6df9a
Jasper Moelker <j.b.moelker@activeids.nl>
2017-03-22 16:50:50 +0100

    cache offline page on install

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,16 @@
-self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+self.addEventListener('install', event => event.waitUntil(
+    caches.open('bs-v1-core')
+        .then(cache => cache.add('/offline/'))
+        .then(self.skipWaiting())
+));
+
+/* check in DevTools > Application > Cache Storage
+ * or check in console using:
+caches.open('bs-v1-core')
+.then(cache => cache.match('/offline/'))
+.then(res => res.text())
+.then(text => console.log(text))
+*/
 
 self.addEventListener('fetch', event => {
     //event.respondWith(new Response('hijacked directly!'));
