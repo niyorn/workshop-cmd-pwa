869f9056bc7398b8851fc3b1f75d14f6ab9ee7ab
Jasper Moelker <j.b.moelker@activeids.nl>
2017-03-22 17:29:46 +0100

    precache core assets on install

--- src/sw.js
+++ src/sw.js
@@ -1,16 +1,15 @@
 self.addEventListener('install', event => event.waitUntil(
     caches.open('bs-v1-core')
-        .then(cache => cache.add('/offline/'))
+        .then(cache => cache.addAll([
+            '/offline/',
+            '/dist/css/bootstrap.css',
+            '/dist/css/bootstrap-theme.css',
+            '/dist/css/fonts.css',
+            '/dist/js/bootstrap.js',
+            '/dist/fonts/glyphicons-halflings-regular.woff2'
+        ]))
         .then(self.skipWaiting())
 ));
-
-/* check in DevTools > Application > Cache Storage
- * or check in console using:
-caches.open('bs-v1-core')
-.then(cache => cache.match('/offline/'))
-.then(res => res.text())
-.then(text => console.log(text))
-*/
 
 self.addEventListener('fetch', event => {
     event.respondWith(
