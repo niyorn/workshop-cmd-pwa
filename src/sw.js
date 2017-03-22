938447a5ded873f7fb97db946bdc21614762364a
Jasper Moelker <j.b.moelker@activeids.nl>
2017-03-22 16:36:20 +0100

    skip waiting on install

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,5 @@
+self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+
 self.addEventListener('fetch', event => {
-    event.respondWith(new Response('hijacked'));
+    event.respondWith(new Response('hijacked directly!'));
 });
